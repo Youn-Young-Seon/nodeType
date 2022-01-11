32c67423262291975b86b1ed752f362e5d392b1c
Youn-Young-Seon <szdbsyyzs@naver.com>
2022-01-11 17:51:25 +0900

    nodeType async

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,77 @@
 const root :Element | null = document.querySelector('#root');
 
-if(root){
-    root.innerHTML = `<div>성공</div>`;
-}else{
-    console.log('ㅗㅗㅗㅗㅗ');
+// function delay(ms: number){
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if(Math.floor(Math.random() * 10 % 2) === 0){
+//                 resolve(console.log('성공'));                
+//             }else{
+//                 reject(console.log('실패'));
+//             }
+//         }, (ms));
+//     })
+// }
+
+// delay(3000)
+// .then((resolve) => {
+//     console.log(resolve);
+// })
+// .catch((reject) => {
+//     console.log(reject);
+// });
+
+// async function main() {
+//     console.log('시작');
+//     try{
+//         const test = await delay(3000);
+//         console.log('만세');        
+//     }catch{
+//         console.log('ㅜㅜ');
+//     }
+// }
+
+// main();
+
+
+function test(x: number, cb: (y: number) => void): void {    
+    cb(x);
 }
+
+function callback(x: number, cb: (y: number) => void): void{
+    setTimeout(function() {
+        console.log('Term');
+        cb(x + x);
+    }, 1000);
+}
+
+function callback2(x: number, cb: (y: number) => void): void{
+    setTimeout(function() {
+        console.log('Term');        
+        cb(x + x);
+    }, 1000);
+}
+
+
+test(1, function(res){
+    console.log(res);
+    callback(res, function(res){
+        console.log(res);
+        callback2(res, function(res){
+            console.log(res);
+        })
+    })
+});
+
+function calculate(a: number, b: number, cb: (result: number) => void): void{
+    setTimeout(() => {
+        cb(a + b);
+    }, 0);
+}
+
+function result(sum: number){
+    console.log(sum);
+}
+
+const r = calculate(10, 20, result);
+const z = r;
 
